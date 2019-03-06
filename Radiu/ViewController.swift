1bff2e8093c938d0365de112ecf6eb354953aff6
Student User <studentuser@D-10-19-167-144.dhcp4.washington.edu>
2019-03-05 17:17:08 -0800

    Added starter files.

---
+++ Radiu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Radiu
+//
+//  Created by Student User on 3/5/19.
+//  Copyright © 2019 University of Washington. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
