38e8335f823c905a76c5b9435c4a385d5c3196ac
Student User <studentuser@D-10-19-167-144.dhcp4.washington.edu>
2019-03-05 18:40:46 -0800

    Added image icons for search tab elements. Added search bar, cancel button, and tab bar.

---
+++ Radiu/SearchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SearchViewController.swift
+//  Radiu
+//
+//  Created by Student User on 3/5/19.
+//  Copyright © 2019 University of Washington. All rights reserved.
+//
+
+import UIKit
+
+class Search: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
