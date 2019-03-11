ad0645f44d364d1d4b2c9cc206c2715a1898430c
Student User <studentuser@D-10-19-220-25.dhcp4.washington.edu>
2019-03-11 13:36:00 -0700

    Adding search functionality. About to remove search bar to add-in programmatically.

--- Radiu/SearchViewController.swift
+++ Radiu/SearchViewController.swift
@@ -13,28 +13,37 @@ import SwiftyJSON
 class Search: UIViewController, UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         //print(data.arrayValue.count)
-        return data.arrayValue.count;
+        return data.count;
     }
     
     @IBOutlet weak var tableView: UITableView!
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "sCell", for: indexPath) as! searchCell
         let data1 = data[indexPath.item]
-        cell.displayName.text = data1["displayName"].stringValue
-        cell.title.text = data1["discription"].stringValue
+        cell.displayName.text = data1.displayName
+        cell.title.text = data1.desc
         cell.profileImage.image = UIImage(named: "hot_ico.png")
         return cell
     }
     
     @IBOutlet weak var searchbar: UISearchBar!
     
+      let searchController = UISearchController(searchResultsController: nil)
     //Downloads JSON file, initializes tableView, then tries to set searchBar color to black
     override func viewDidLoad() {
         super.viewDidLoad()
         download(url, self)
+        //Search taken from https://www.raywenderlich.com/472-uisearchcontroller-tutorial-getting-started
+        // Setup the Search Controller
+        searchController.searchResultsUpdater = self as! UISearchResultsUpdating
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search..."
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
         tableView.delegate = self
         tableView.dataSource = self
         searchbar.barTintColor = UIColor.black
+       
         // Do any additional setup after loading the view.
     }
     
@@ -46,12 +55,16 @@ class Search: UIViewController, UITableViewDelegate, UITableViewDataSource {
      * let savedJSON = UserDefaults.standard.string(forKey: "searchStreams")
      */
     //var data: Array<searchProperties> = []
-    var data: JSON = []
+    var data: Array<searchProperties> = []
     let url = "https://audio-api.kjgoodwin.me/v1/audio/channels/all"
     func download(_ url: String, _ VC: UIViewController)  {
         Alamofire.request(url).responseJSON{response in
             if response.result.value != nil {
-                self.data = JSON(response.result.value as Any)
+                let data = JSON(response.result.value as Any)
+                for d in data.arrayValue {
+                    let newStruct = searchProperties(id: d["id"].stringValue, desc: d["discription"].stringValue, genre: d["genre"].stringValue, createdAt: d["createdAt"].stringValue, creator: d["creator"].intValue, active: d["active"].boolValue, displayName: d["displayName"].stringValue)
+                    self.data.append(newStruct)
+                }
                 self.tableView.reloadData()
                 
                 //We got the data, now we'll save the data in its state as a String
@@ -66,6 +79,24 @@ class Search: UIViewController, UITableViewDelegate, UITableViewDataSource {
             }
         }
     }
+    
+    //Taken from https://www.raywenderlich.com/472-uisearchcontroller-tutorial-getting-started
+    // MARK: - Private instance methods
+
+    func searchBarIsEmpty() -> Bool {
+        // Returns true if the text is empty or nil
+        return searchController.searchBar.text?.isEmpty ?? true
+    }
+    
+    var filteredData: Array<searchProperties> = []
+    func filterContentForSearchText(_ searchText: String, scope: String = "All") {
+        filteredData = data.filter({( search : searchProperties) -> Bool in
+            return search.displayName.lowercased().contains(searchText.lowercased())
+        })
+        
+        tableView.reloadData()
+    }
+
     /*
     // MARK: - Navigation
 
@@ -76,6 +107,14 @@ class Search: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     */
 
+}
+
+//Taken from https://www.raywenderlich.com/472-uisearchcontroller-tutorial-getting-started
+extension Search: UISearchResultsUpdating {
+    // MARK: - UISearchResultsUpdating Delegate
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearchText(searchController.searchBar.text!)
+    }
 }
 
 struct searchProperties {
