06c6501022f5ac33e41fc0c506594ba783861bf1
Student User <studentuser@Students-MBP.hsd1.wa.comcast.net>
2019-03-10 21:55:30 -0700

    Setting up the tableview

--- Radiu/SearchViewController.swift
+++ Radiu/SearchViewController.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
-class Search: UIViewController {
+class Search: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "qcell", for: indexPath) as! searchCell
+        cell.name.text = "Default"
+        cell.title.text = "This is a broadcast."
+        cell.imageView?.image = UIImage(named: "hot_ico.png")
+        print("hi")
+        return cell
+    }
+    
+    @IBOutlet weak var searchbar: UISearchBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        searchbar.barTintColor = UIColor.black
         // Do any additional setup after loading the view.
     }
     
@@ -27,4 +43,10 @@ class Search: UIViewController {
     }
     */
 
+}
+
+class searchCell: UITableViewCell {
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var title: UILabel!
 }
