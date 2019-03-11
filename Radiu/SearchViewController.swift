910b62b70aac336671be1ab9556c7e344ddbc3cd
Student User <studentuser@Students-MBP.hsd1.wa.comcast.net>
2019-03-11 00:00:03 -0700

    Added functionality to download JSON from URL. Will probably want to change from storing data to simply using.

--- Radiu/SearchViewController.swift
+++ Radiu/SearchViewController.swift
@@ -34,7 +34,31 @@ class Search: UIViewController, UITableViewDelegate, UITableViewDataSource {
         // Do any additional setup after loading the view.
     }
     
-
+    /*
+     * Downloads JSON file from given url and saves to UserDefaults
+     * Copied from iQuiz project
+     * Change to return a JSON object.
+     * Should probably also move to another class/file
+     * let savedJSON = UserDefaults.standard.string(forKey: "searchStreams")
+     */
+    func download(_ url: String, _ VC: UIViewController) {
+        Alamofire.request(url).responseJSON{response in
+            if response.result.value != nil {
+                let data = JSON(response.result.value as Any)
+                
+                //We got the data, now we'll save the data in its state as a String
+                UserDefaults.standard.set(data.rawString(), forKey: "searchStreams")
+                //now that we saved we can parse this inforamtion into another function
+                
+            }
+            else {
+                let alert = UIAlertController(title: "Download Failed.", message: "Please try again later.", preferredStyle: UIAlertController.Style.alert)
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+                VC.present(alert, animated: true, completion: nil)
+                
+            }
+        }
+    }
     /*
     // MARK: - Navigation
 
